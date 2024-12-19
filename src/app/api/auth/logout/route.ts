964fb874745e1778b9d472ee4603812c42393691
vially <viallymboma@users.noreply.github.com>
2024-12-19 13:28:16 +0100

    changing how to logout

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,15 +1,21 @@
-import { NextResponse } from 'next/server';
+import {
+  NextRequest,
+  NextResponse,
+} from 'next/server';
 
-export async function GET() {
+export async function GET(request: NextRequest) {
+  const token = request.cookies.get('jwt')?.value;
   const response = NextResponse.json({ message: 'Logged out successfully' });
 
-  // Clear the `jwt` cookie
-  response.cookies.set('jwt', '', {
-    httpOnly: true, // Ensure the cookie cannot be accessed via JavaScript
-    maxAge: 0, // Expire the cookie immediately
-    // sameSite: 'strict', // Uncomment if needed
-    secure: true, // Uncomment if your app is served over HTTPS
-  });
+  // // Clear the `jwt` cookie
+  // response.cookies.set('jwt', '', {
+  //   httpOnly: true, // Ensure the cookie cannot be accessed via JavaScript
+  //   maxAge: 0, // Expire the cookie immediately
+  //   // sameSite: 'strict', // Uncomment if needed
+  //   secure: true, // Uncomment if your app is served over HTTPS
+  // });
+  // Manually set the Set-Cookie header to clear the cookie
+  response.headers.set('Set-Cookie', `jwt=; Path=/; Max-Age=0; HttpOnly; Secure; SameSite=Strict`);
 
   return response;
 }
